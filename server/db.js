d9b464a569b3d1a5f986e03f8c393e6b7bb0cba0
Va1bhav07 <vaibhavarora8401@gmail.com>
2024-06-05 15:16:53 -0400

    added vercel json file

--- server/db.js
+++ server/db.js
@@ -4,11 +4,8 @@ const { MONGODB_URI } = process.env;
 // Connect Mongo DB
 const connectDB = async () => {
   try {
-    //const conn = await mongoose.connect(MONGODB_URI);
-    const conn = await mongoose.connect(
-      "mongodb+srv://capstone:capstone123@cluster0.rzqevhy.mongodb.net/learningplatform?retryWrites=true&w=majority"
-    );
-    
+    const conn = await mongoose.connect(MONGODB_URI);
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(`Error connecting to MongoDB: :>> ${error} `);
