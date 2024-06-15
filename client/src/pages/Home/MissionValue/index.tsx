d57ad3bb01d086fb54dcb2d7a5803b284795d104
Vaibhav Arora <vaibhavarora8401@gmail.com>
2024-06-15 05:13:32 -0400

    some txt fixes

--- client/src/pages/Home/MissionValue/index.tsx
+++ client/src/pages/Home/MissionValue/index.tsx
@@ -8,7 +8,7 @@ function MissionValue() {
         <h4 className="fw-bold primary-text-color text-center mb-0">
           Our Mission
         </h4>
-        <div style={{ fontSize: '18px' }}>
+        <div className="text-center text-lg-start" style={{ fontSize: '18px' }}>
           <p className="px-0 py-3 p-lg-4 mb-0">
             VASchool is an EdTech platform dedicated to empowering aspiring
             learners and coders to create impactful projects using the latest
@@ -17,7 +17,7 @@ function MissionValue() {
             approach, focusing on practical, hands-on learning experiences for
             students.
           </p>
-          <p className="px-0 py-3 p-4 pt-0">
+          <p className="px-0 py-3 p-lg-4 pt-lg-0">
             With courses like VASchool React and VASchool Frontend System
             Design, he is committed to building a community of exceptional
             front-end developers. We are devoted to promoting continuous growth
@@ -25,18 +25,18 @@ function MissionValue() {
             instruction and a nurturing learning environment. This VASchool UI
             is inspired by NamasteDev.com.
           </p>
-        </div>
-        <h4 className="fw-bold primary-text-color text-center mb-0">
-          Our Value
-        </h4>
-        <div style={{ fontSize: '18px' }}>
-          <p className="px-0 py-3 p-4 mb-0">
+
+          <h4 className="fw-bold primary-text-color text-center mb-0">
+            Our Value
+          </h4>
+
+          <p className="px-0 py-3 p-lg-4 mb-0">
             Our Vision is to empower a generation of developers to shape and
             contribute to the digital future. The VASchool platform is dedicated
             to providing easy access to premium learning resources for the tech
             community, enabling them to upskill and advance in their careers.
           </p>
-          <p className="px-0 py-3 p-4 pt-0">
+          <p className="px-0 py-3 p-lg-4 pt-lg-0">
             By offering top-tier educational content, we aim to nurture talent
             and foster innovation, ensuring that our developers are equipped to
             meet the evolving demands of the tech industry and drive progress
